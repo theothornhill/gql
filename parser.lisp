6a274d35835af8222680a8475a6dd99192a9865d
Theodor Thornhill <theo@thornhill.no>
2021-04-05 13:50:25 +0200

    Flesh out utility functions for parsing

--- parser.lisp
+++ parser.lisp
@@ -37,39 +37,57 @@
 (defgeneric loc (parser token)
   (:documentation "Returns a location object, used to identify the place in the source that
 created a given parsed object."))
+
 (defgeneric peek (parser kind)
   (:documentation "Determines if the next token is of a given KIND."))
+
 (defgeneric expect-token (parser kind)
   (:documentation "If the next token is of the given kind, return that token after advancing the lexer.
 Otherwise, do not change the parser state and throw an error."))
+
 (defgeneric expect-optional-token (parser kind)
   (:documentation "If the next token is of the given kind, return that token after advancing the lexer.
 Otherwise, do not change the parser state and return nil."))
+
 (defgeneric expect-keyword (parser value)
   (:documentation "If the next token is of the given kind, return that token after advancing the lexer.
 Otherwise, do not change the parser state and throw an error."))
+
 (defgeneric expect-optional-keyword (parser value)
   (:documentation "If the next token is of the given kind, return that token after advancing the lexer.
 Otherwise, do not change the parser state and return nil."))
+
 (defgeneric unexpected (parser token)
   (:documentation "Helper function for creating an error when an unexpected lexed token is encountered."))
+
 (defgeneric any (parser open-kind parse-fn close-kind)
-  (:documentation "Returns a possibly empty list of parse nodes, determined by the parseFn.
-This list begins with a lex token of openKind and ends with a lex token of closeKind.
-Advances the parser to the next lex token after the closing token."))
+  (:documentation "Returns a possibly empty list of parse nodes, determined by the PARSE-FN.
+
+This list begins with a lex token of OPEN-KIND and ends with a lex token of
+CLOSE-KIND.  Advances the parser to the next lex token after the closing token."))
+
 (defgeneric optional-many (parser open-kind parse-fn close-kind)
-  (:documentation "Returns a list of parse nodes, determined by the parseFn.
-It can be empty only if open token is missing otherwise it will always return non-empty list
-that begins with a lex token of openKind and ends with a lex token of closeKind.
-Advances the parser to the next lex token after the closing token."))
+  (:documentation "Returns a list of parse nodes, determined by the PARSE-FN.
+
+It can be empty only if open token is missing.  Otherwise it will always return
+a non-empty list that begins with a lex token of OPEN-KIND and ends with a lex
+token of CLOSE-KIND.  Advances the parser to the next lex token after the
+closing token."))
+
 (defgeneric many (parser open-kind parse-fn close-kind)
   (:documentation "Returns a non-empty list of parse nodes, determined by the parseFn.
-This list begins with a lex token of openKind and ends with a lex token of closeKind.
-Advances the parser to the next lex token after last item in the list."))
+
+This list begins with a lex token of OPEN-KIND and ends with a lex token of
+CLOSE-KIND.  Advances the parser to the next lex token after last item in the
+list."))
+
 (defgeneric delimited-many (parser delimiter-kind parse-fn)
-  (:documentation "Returns a possibly empty list of parse nodes, determined by the parseFn.
-This list begins with a lex token of openKind and ends with a lex token of tokenKind.
-Advances the parser to the next lex token after the closing token."))
+  (:documentation "Returns a possibly empty list of parse nodes, determined by the PARSE-FN
+
+This list may begin with a lex token of DELIMITER-KIND followed by items
+separated by lex tokens of TOKEN-KIND.  Advances the parser to the next lex
+token after last item in the list."))
+
 (defgeneric parse (parser node-type)
   (:documentation "Parse node of NODE-tYPE with parser PARSER."))
 
@@ -86,8 +104,8 @@ Advances the parser to the next lex token after the closing token."))
 (defmethod peek ((parser parser) kind)
   (with-slots (lexer) parser
     (with-slots (token) lexer
-      (eq (kind token) kind))))
-
+      (with-lexer-token
+          (eq (kind token) kind)))))
 
 (defmethod expect-token ((parser parser) kind)
   (with-slots (lexer) parser
@@ -120,10 +138,33 @@ Advances the parser to the next lex token after the closing token."))
     (gql-error "Unexpected token: ~a" token)))
 
 (defmethod any ((parser parser) open-kind parse-fn close-kind)
-  nil)
+  (expect-token parser open-kind)
+  (loop
+    with nodes = nil
+    until (expect-optional-token parser close-kind)
+    do (push (funcall parse-fn) nodes)
+    finally (return (nreverse nodes))))
+
 (defmethod optional-many ((parser parser) open-kind parse-fn close-kind)
-  nil)
+  (when (expect-optional-token open-kind)
+    (loop
+      with nodes = nil
+      do (push (funcall parse-fn) nodes)
+         (when (expect-optional-token parser close-kind)
+           (return-from optional-many (nreverse nodes))))))
+
 (defmethod many ((parser parser) open-kind parse-fn close-kind)
-  nil)
-(defmethod delimited-many ((parser parser) open-kind parse-fn close-kind)
-  nil)
+  (expect-token parser open-kind)
+  (loop
+    with nodes = nil
+    do (push (funcall parse-fn) nodes)
+       (when (expect-optional-token parser close-kind)
+         (return-from many (nreverse nodes)))))
+
+(defmethod delimited-many ((parser parser) delimiter-kind parse-fn)
+  (expect-token parser delimiter-kind)
+  (loop
+    with nodes = nil
+    do (push (funcall parse-fn) nodes)
+       (unless (expect-optional-token parser delimiter-kind)
+         (return-from delimited-many (nreverse nodes)))))
