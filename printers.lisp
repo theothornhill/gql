26731b8c3b2229ac71b40de180d22a7cf326f043
Theodor Thornhill <theo@thornhill.no>
2021-04-18 23:21:52 +0200

    Make stream optional

--- printers.lisp
+++ printers.lisp
@@ -8,38 +8,38 @@
 ;; query { x: String }
 ;; NIL
 
-(defgeneric print-node (node stream)
+(defgeneric print-node (node &optional stream)
   (:documentation "print a node as a valid GrqphQL statement."))
 
-(defmethod print-node ((node document) stream)
+(defmethod print-node ((node document) &optional (stream nil))
   (let ((definitions
-          (mapcar (lambda (x) (format nil "~a" (print-node x nil)))
+          (mapcar (lambda (x) (format nil "~a" (print-node x)))
                   (definitions node))))
     (format stream "~{~a~}" definitions)))
 
-(defmethod print-node ((node operation-definition) stream)
+(defmethod print-node ((node operation-definition) &optional (stream nil))
   (format stream "~a ~@[~a~] ~a"
           (operation node)
-          (when (name node) (print-node (name node) nil))
+          (when (name node) (print-node (name node)))
           ;; (variable-definitions node)
           ;; (directives node)
-          (print-node (selection-set node) nil)
+          (print-node (selection-set node))
           ;; (definitions node)
           ))
 
-(defmethod print-node ((node name) stream)
+(defmethod print-node ((node name) &optional (stream nil))
   (format stream "~@[~a~]" (name node)))
 
-(defmethod print-node ((node selection-set) stream)
+(defmethod print-node ((node selection-set) &optional (stream nil))
   (let ((selections
-          (mapcar (lambda (x) (format nil "~a" (print-node x nil)))
+          (mapcar (lambda (x) (format nil "~a" (print-node x)))
                   (selections node))))
     (format stream "{ ~{~a~} }" selections)))
 
-(defmethod print-node ((node field) stream)
+(defmethod print-node ((node field) &optional (stream nil))
   (format stream "~a: ~a"
-          (print-node (alias node) nil)
-          (print-node (name node) nil)))
+          (print-node (alias node))
+          (print-node (name node))))
 
-(defmethod print-node ((node argument) stream)
+(defmethod print-node ((node argument) &optional (stream nil))
   (format stream "~a: ~a" (name node) (value node)))
