3f78d3d9b20cf67d4a16f2e3521beac68cdbe500
Theodor Thornhill <theo@thornhill.no>
2021-11-18 22:25:55 +0100

    Don't use double colon

--- test.lisp
+++ test.lisp
@@ -1,6 +1,6 @@
 (ql:quickload :gql/tests)
 
-(multiple-value-bind (foo bar fails) (rove::run :gql/tests)
+(multiple-value-bind (foo bar fails) (rove:run :gql/tests)
   (when fails
     (sb-ext:exit :code 1)))
 
