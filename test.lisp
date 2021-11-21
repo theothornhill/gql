3e435de5f2dce80a9e3e95556974578007ed8401
Theodor Thornhill <theo@thornhill.no>
2021-11-21 23:00:23 +0100

    Simplify test-running on ci

--- test.lisp
+++
@@ -1,7 +0,0 @@
-(ql:quickload :gql-tests)
-
-(multiple-value-bind (foo bar fails) (rove:run :gql-tests)
-  (when fails
-    (sb-ext:exit :code 1)))
-
-
--- test.sh
+++ test.sh
@@ -2,4 +2,10 @@
 
 set -e
 
-sbcl --non-interactive --load test.lisp
+sbcl --non-interactive \
+     --load gql.asd \
+     --eval '(ql:quickload :rove)' \
+     --eval '(ql:quickload :gql)' \
+     --eval '(ql:quickload :gql-tests)' \
+     --eval '(ql:quickload :gql-tests)' \
+     --eval '(multiple-value-bind (foo bar fails) (rove:run :gql-tests) (when fails (sb-ext:exit :code 1)))'
